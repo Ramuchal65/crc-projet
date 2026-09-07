759c19a8fe9a790732bb62fcdd3d12b17f1de4a6
Ramuchal65 <floran.barelli@gmail.com>
2026-09-07 11:58:59 +0200

    Add files via upload

--- crc-pilotage/app/(app)/layout.tsx
+++ crc-pilotage/app/(app)/layout.tsx
@@ -1,14 +1,9 @@
-import Sidebar from "@/components/Sidebar";
+import AppShell from "@/components/AppShell";
 
 export default function AppLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <div className="flex min-h-screen">
-      <Sidebar />
-      <main className="flex-1 px-8 py-8 max-w-[1600px] min-w-0">{children}</main>
-    </div>
-  );
+  return <AppShell>{children}</AppShell>;
 }
